16ba11a3a3773ef7150148af09af18b01b4a4a49
Yash Goyal <yashgo0018@gmail.com>
2023-01-22 01:07:35 +0530

    added dotenv

--- Contract/hardhat.config.ts
+++ Contract/hardhat.config.ts
@@ -1,11 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
-  defaultNetwork: "hyperspace",
   networks: {
     hyperspace: {
       chainId: 3141,
