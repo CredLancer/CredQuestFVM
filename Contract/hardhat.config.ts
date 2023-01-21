49cde3310b9175f79a4eaf3dee1c78c203c891bc
Yash Goyal <yashgo0018@gmail.com>
2023-01-21 23:52:54 +0530

    updated the hardhat config

--- Contract/hardhat.config.ts
+++ Contract/hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "hyperspace",
+  networks: {
+    hyperspace: {
+      chainId: 3141,
+      url: "https://api.hyperspace.node.glif.io/rpc/v1",
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };
 
 export default config;
