ef337dfe4c7bd8eba43ac61aad66481d4793cae6
Yash Goyal <yashgo0018@gmail.com>
2023-01-23 12:30:38 +0530

    added deployment script

--- Contract/scripts/deploy.ts
+++ Contract/scripts/deploy.ts
@@ -1,4 +1,20 @@
-async function main() {}
+import { ethers } from "hardhat";
+
+async function main() {
+  // deploy the organizations contract
+  const OrganizationController = await ethers.getContractFactory(
+    "OrganizationController"
+  );
+  const organizationController = await OrganizationController.deploy();
+  await organizationController.deployed();
+
+  // deploy the quests contract
+  const QuestController = await ethers.getContractFactory("QuestController");
+  const questController = await QuestController.deploy(
+    organizationController.address
+  );
+  await questController.deployed();
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
