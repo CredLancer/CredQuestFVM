3c97fc5f40f578761a6a62e3271d833e8ad61c0f
Yash Goyal <yashgo0018@gmail.com>
2023-01-23 12:27:40 +0530

    added work submission

--- Contract/contracts/QuestController.sol
+++ Contract/contracts/QuestController.sol
@@ -12,11 +12,18 @@ contract QuestController is Ownable, Pausable {
         Rejected
     }
 
+    enum QuestStatus {
+        Open,
+        Closed,
+        Awarded
+    }
+
     struct Quest {
         uint256 id;
         bytes cid;
         uint256 reward;
         uint256 orgId;
+        QuestStatus status;
     }
 
     struct Proposal {
@@ -25,6 +32,7 @@ contract QuestController is Ownable, Pausable {
         address proposer;
         uint256 questId;
         ProposalStatus status;
+        bytes workCID;
     }
 
     mapping(uint256 => Quest) public quests; // f: (questId) -> quest
@@ -37,21 +45,28 @@ contract QuestController is Ownable, Pausable {
     OrganizationController public organizationController;
 
     event QuestCreated(
-        uint256 questId,
+        uint256 indexed questId,
+        uint256 indexed organizationId,
         bytes questCID,
-        uint256 reward,
-        uint256 organizationId
+        uint256 reward
     );
     event ProposalCreated(
-        uint256 proposalId,
-        bytes proposalCID,
-        address proposer,
-        uint256 questId
+        uint256 indexed questId,
+        uint256 indexed proposalId,
+        address indexed proposer,
+        bytes proposalCID
     );
     event ProposalStatusChanged(
-        uint256 proposalId,
+        uint256 indexed questId,
+        uint256 indexed proposalId,
         ProposalStatus oldStatus,
         ProposalStatus newStatus
+    );
+    event WorkSubmitted(
+        uint256 indexed questId,
+        uint256 indexed proposalId,
+        address indexed worker,
+        bytes workCID
     );
 
     error InvalidOrganizationId();
@@ -62,6 +77,9 @@ contract QuestController is Ownable, Pausable {
     error ProposalAlreadySent();
     error ProposalAlreadyInSameStatus();
     error ProposalAlreadyRejected();
+    error QuestNotOpen();
+    error ProposalNotFound();
+    error ProposalNotAccepted();
 
     constructor(OrganizationController _organizationController) {
         organizationController = _organizationController;
@@ -87,23 +105,22 @@ contract QuestController is Ownable, Pausable {
         if (msg.value != reward) revert InvalidValue();
         // TODO: Also do some server signature validation before creating a quest
         // TODO: Also verify the cid
-        uint256 questId = totalQuests++;
+        uint256 questId = ++totalQuests;
         quests[questId] = Quest({
             id: questId,
             cid: questCID,
             reward: reward,
-            orgId: orgId
+            orgId: orgId,
+            status: QuestStatus.Open
         });
-        emit QuestCreated(questId, questCID, reward, orgId);
+        emit QuestCreated(questId, orgId, questCID, reward);
     }
 
-    function sendProposal(uint256 questId, bytes calldata proposalCID)
-        public
-        payable
-    {
+    function sendProposal(uint256 questId, bytes calldata proposalCID) public {
         // TODO: The organization admin should not be able to send proposal
         if (!questExists(questId)) revert InvalidQuestId();
         if (proposalIds[questId][msg.sender] != 0) revert ProposalAlreadySent();
+        if (quests[questId].status != QuestStatus.Open) revert QuestNotOpen();
         // TODO: verify the signature
         // TODO: verify the cid
         uint256 proposalId = ++totalProposals;
@@ -112,10 +129,24 @@ contract QuestController is Ownable, Pausable {
             cid: proposalCID,
             proposer: msg.sender,
             questId: questId,
-            status: ProposalStatus.Proposed
+            status: ProposalStatus.Proposed,
+            workCID: bytes("")
         });
         proposalIds[questId][msg.sender] = proposalId;
-        emit ProposalCreated(proposalId, proposalCID, msg.sender, questId);
+        emit ProposalCreated(questId, proposalId, msg.sender, proposalCID);
+    }
+
+    function submitWork(uint256 questId, bytes calldata workCID) public {
+        if (!questExists(questId)) revert InvalidQuestId();
+        if (quests[questId].status != QuestStatus.Open) revert QuestNotOpen();
+        uint256 proposalId = proposalIds[questId][msg.sender];
+        if (proposalId == 0) revert ProposalNotFound();
+        Proposal storage proposal = proposals[proposalId];
+        if (proposal.status != ProposalStatus.Accepted)
+            revert ProposalNotAccepted();
+        // TODO: check the cid
+        proposal.workCID = workCID;
+        emit WorkSubmitted(questId, proposalId, msg.sender, workCID);
     }
 
     function acceptProposal(uint256 proposalId) public {
@@ -127,16 +158,17 @@ contract QuestController is Ownable, Pausable {
     }
 
     function proposalExists(uint256 proposalId) public view returns (bool) {
-        return proposalId < totalProposals;
+        return proposalId <= totalProposals && proposalId != 0;
     }
 
     function questExists(uint256 questId) public view returns (bool) {
-        return questId < totalQuests;
+        return questId <= totalQuests && questId != 0;
     }
 
     function _changeProposalStatus(uint256 proposalId, ProposalStatus newStatus)
         private
     {
+        // TODO: check if quest is closed first
         if (!proposalExists(proposalId)) revert InvalidProposalId();
         Proposal memory proposal = proposals[proposalId];
         uint256 orgId = quests[proposal.questId].orgId;
@@ -145,6 +177,11 @@ contract QuestController is Ownable, Pausable {
         if (proposal.status == newStatus) revert ProposalAlreadyInSameStatus();
         ProposalStatus oldStatus = proposal.status;
         proposals[proposalId].status = newStatus;
-        emit ProposalStatusChanged(proposalId, oldStatus, newStatus);
+        emit ProposalStatusChanged(
+            proposal.questId,
+            proposalId,
+            oldStatus,
+            newStatus
+        );
     }
 }
