2b751d73fc1274a5c80906618f3fe910f1a11c1c
Yash Goyal <yashgo0018@gmail.com>
2023-01-24 13:21:19 +0530

    fixed the deployment script

--- Contract/scripts/deploy.ts
+++ Contract/scripts/deploy.ts
@@ -1,19 +1,33 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  // get provider
+  const { provider } = ethers;
+
   // deploy the organizations contract
+  console.log("Deploying the organization contract");
   const OrganizationController = await ethers.getContractFactory(
     "OrganizationController"
   );
-  const organizationController = await OrganizationController.deploy();
-  await organizationController.deployed();
+  const organizationController = await OrganizationController.deploy({
+    maxPriorityFeePerGas: await provider.send("eth_maxPriorityFeePerGas", []),
+  });
+  console.log(
+    `The organization contract is deployed to address: ${organizationController.address}`
+  );
 
   // deploy the quests contract
+  console.log("Deploying the quest contract");
   const QuestController = await ethers.getContractFactory("QuestController");
   const questController = await QuestController.deploy(
-    organizationController.address
+    organizationController.address,
+    {
+      maxPriorityFeePerGas: await provider.send("eth_maxPriorityFeePerGas", []),
+    }
   );
-  await questController.deployed();
+  console.log(
+    `The quest contract is deployed to address: ${questController.address}`
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
