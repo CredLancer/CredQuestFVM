a798053261ce71f2e2bfa5a859f0135c5500c25d
Yash Goyal <yashgo0018@gmail.com>
2023-01-24 15:51:19 +0530

    added foundation code for added web3 event listeners

--- BackEnd/src/config.ts
+++ BackEnd/src/config.ts
@@ -2,3 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = parseInt(process.env.PORT || "8000");
+export const ORGANIZATION_CONTROLLER_ADDRESS =
+  process.env.ORGANIZATION_CONTROLLER_ADDRESS || "";
+export const QUEST_CONTROLLER_ADDRESS =
+  process.env.QUEST_CONTROLLER_ADDRESS || "";
+export const WEB3_RPC_URI = process.env.WEB3_RPC_URI || "";
