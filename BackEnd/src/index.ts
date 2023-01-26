549b12c69f3d5670810a1309804e41b121f7720e
Yash Goyal <yashgo0018@gmail.com>
2023-01-26 19:59:01 +0530

    added event listeners for the organization contract

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT } from "./config";
 import cors from "cors";
+import web3EventListeners from "./web3EventListeners";
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
 });
+
+web3EventListeners();
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- BackEnd/src/web3EventListeners/index.ts
+++ BackEnd/src/web3EventListeners/index.ts
@@ -1,3 +1,4 @@
+import { OrganizationDetailUpdateType, PrismaClient } from "@prisma/client";
 import { ethers } from "ethers";
 import ORGANIZATION_CONTROLLER_ABI from "../abi/OrganizationController.json";
 import QUEST_CONTROLLER_ABI from "../abi/QuestController.json";
@@ -7,8 +8,12 @@ import {
   WEB3_RPC_URI,
 } from "../config";
 import { OrganizationController, QuestController } from "../typechain-types";
+import organizationAdminChangeHandler from "./eventHandlers/organizationAdminChangeHandler";
+import organizationCreatedHandler from "./eventHandlers/organizationCreatedHandler";
+import organizationImageCIDChangeHandler from "./eventHandlers/organizationImageCIDChangeHandler";
+import organizationNameChangeHandler from "./eventHandlers/organizationNameChangeHandler";
 
-const provider = new ethers.providers.WebSocketProvider(WEB3_RPC_URI);
+const provider = new ethers.providers.JsonRpcProvider(WEB3_RPC_URI);
 
 const organizationController = new ethers.Contract(
   ORGANIZATION_CONTROLLER_ADDRESS,
@@ -22,4 +27,99 @@ const questController = new ethers.Contract(
   provider
 ) as QuestController;
 
-export default async function web3EventListeners() {}
+const prisma = new PrismaClient();
+
+const approxBlockTime = 30;
+const blocksInADay = ((24 * 60 * 60) / approxBlockTime) | 0;
+
+async function main(currBlockNumber: number) {
+  const LOWEST_BLOCK = currBlockNumber - blocksInADay - 1;
+  console.log(currBlockNumber);
+  console.log(LOWEST_BLOCK);
+
+  const organizationCreatedFilter =
+    organizationController.filters.OrganizationCreated();
+  const adminChangedFilter = organizationController.filters.AdminChanged();
+  const organizationNameChangedFilter =
+    organizationController.filters.OrganizationNameChanged();
+  const organizationImageCIDChangedFilter =
+    organizationController.filters.OrganizationImageCIDChanged();
+
+  {
+    const lastCreatedOrganization = await prisma.organization.findFirst({
+      orderBy: { blockNumber: "desc" },
+    });
+    const lastBlock =
+      lastCreatedOrganization && lastCreatedOrganization.blockNumber
+        ? lastCreatedOrganization.blockNumber
+        : LOWEST_BLOCK;
+    const eventsEmitted = await organizationController.queryFilter(
+      organizationCreatedFilter,
+      lastBlock
+    );
+
+    for (const eventEmitted of eventsEmitted) {
+      await organizationCreatedHandler(eventEmitted);
+    }
+  }
+
+  {
+    const lastAdminChanged = await prisma.organizationDetailUpdate.findFirst({
+      where: { type: OrganizationDetailUpdateType.AdminChange },
+      orderBy: { blockNumber: "desc" },
+    });
+    const lastBlock = lastAdminChanged
+      ? lastAdminChanged.blockNumber
+      : LOWEST_BLOCK;
+    const eventsEmitted = await organizationController.queryFilter(
+      adminChangedFilter,
+      lastBlock
+    );
+
+    for (const eventEmitted of eventsEmitted) {
+      await organizationAdminChangeHandler(eventEmitted);
+    }
+  }
+
+  {
+    const lastNameChanged = await prisma.organizationDetailUpdate.findFirst({
+      where: { type: OrganizationDetailUpdateType.NameChange },
+      orderBy: { blockNumber: "desc" },
+    });
+    const lastBlock = lastNameChanged
+      ? lastNameChanged.blockNumber
+      : LOWEST_BLOCK;
+    const eventsEmitted = await organizationController.queryFilter(
+      organizationNameChangedFilter,
+      lastBlock
+    );
+
+    for (const eventEmitted of eventsEmitted) {
+      await organizationNameChangeHandler(eventEmitted);
+    }
+  }
+
+  {
+    const lastImageCIDChanged = await prisma.organizationDetailUpdate.findFirst(
+      {
+        where: { type: OrganizationDetailUpdateType.ImageCIDChange },
+        orderBy: { blockNumber: "desc" },
+      }
+    );
+    const lastBlock = lastImageCIDChanged
+      ? lastImageCIDChanged.blockNumber
+      : LOWEST_BLOCK;
+    const eventsEmitted = await organizationController.queryFilter(
+      organizationImageCIDChangedFilter,
+      lastBlock
+    );
+
+    for (const eventEmitted of eventsEmitted) {
+      await organizationImageCIDChangeHandler(eventEmitted);
+    }
+  }
+}
+
+export default async function web3EventListeners() {
+  provider.on("block", main);
+}
