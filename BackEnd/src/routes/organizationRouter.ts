b8f4deb998bfc852c2ae3808a58437103fa4fd9d
Yash Goyal <yashgo0018@gmail.com>
2023-01-29 23:26:48 +0530

    added middlewares

--- BackEnd/src/routes/organizationRouter.ts
+++ BackEnd/src/routes/organizationRouter.ts
@@ -163,4 +163,52 @@ organizationRouter.get("/admin/:admin", async (req, res) => {
   res.json({ org });
 });
 
+organizationRouter.put(
+  "/imageCID/:orgId",
+  file("image", "image"),
+  body("signature").isEthereumAddress(),
+  body("signer").isEthereumAddress(),
+  validate,
+  async (req, res) => {
+    const { orgId } = req.params;
+    const image = req.file;
+    const { signature: userSignature, signer } = req.body;
+    if (!image) return res.status(400).json({ message: "file not found" });
+    if (!image.mimetype.toLowerCase().includes("image"))
+      return res.status(400).json({ message: "file not a image" });
+    const organization = await prisma.organization.findUnique({
+      where: { id: orgId },
+    });
+    if (!organization)
+      return res.status(404).json({ message: "Organization not found" });
+    if (organization.admin != signer)
+      return res.status(401).json({ message: "only admins are allowed" });
+    const response = await uploadToIPFS(image.buffer);
+    const imageCID = `0x${new CID(response.Hash)
+      .toV1()
+      .toString("base16")
+      .substring(1)}`;
+    if (organization.imageCID == imageCID)
+      return res.status(400).json({ message: "image not changed" });
+    const nonce = await getNonce();
+
+    // check the signature
+
+    const signature = await signForOrganizationImageCIDUpdate({
+      orgId,
+      imageCID,
+      nonce,
+    });
+    await prisma.signature.create({
+      data: {
+        nonce: Number(nonce),
+        signature,
+        user: signer,
+        type: SignatureType.OrganizationImageCIDChange,
+      },
+    });
+    res.json({ nonce, signature, imageCID });
+  }
+);
+
 export default organizationRouter;
