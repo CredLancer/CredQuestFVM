dd5e175b7966e0bd043c00923211514f1ef32588
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2023-02-06 09:53:10 +0100

    colpleted workflow

--- Frontend/pages/dashboard.tsx
+++ Frontend/pages/dashboard.tsx
@@ -30,54 +30,32 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useAccount, useSendTransaction } from "wagmi";
 import { BigNumber } from "@ethersproject/bignumber";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { SelectRoleModal } from "../components/Modals/SelectRole";
 import { useRouter } from "next/router";
 import { BackHouseLayout } from "../components/Page/BackOfHouseLayout";
 import { DashboardLayout } from "../components/Page/DashboardLayout";
 import { useQuery } from "react-query";
-import { LancerService } from "../services";
-
-interface MemberCredentials {
-  id: "5";
-  holderAddress: "0x074708eb3868d505d9309f309B884aeb4E7a0ba8";
-  transactionHash: "0xf3ba3d0bb94f905180e4db9390d68d06e6e6e00e74da4047f014cb223a69a614";
-  blockNumber: 60716;
-  transactionIndex: 2;
-  logIndex: 0;
-}
+import {
+  LancerService,
+  OrganizationService,
+  ProposalService,
+  QuestService,
+} from "../services";
+import { ProposalStatus } from "../utils/models";
 
 interface CompletedQuest {
   cid: string;
   title: string;
   description: string;
-  skills: [
-    {
-      title: "Analytical Thinking";
-    },
-    {
-      title: "Collaboration";
-    },
-    {
-      title: "Communication";
-    },
-    {
-      title: "Organization";
-    },
-    {
-      title: "Planning";
-    },
-    {
-      title: "Problem-solving";
-    }
-  ];
-  id: "5";
-  value: "100000000000000000";
-  status: "Awarded";
-  questCID: "0x01701220f894771783732e054c8c0c7b7666c1cca093f0d8b45f91072f132f0188018599";
-  orgId: "4";
-  deadline: "2023-02-10T19:20:00.000Z";
-  blockNumber: 58722;
+  skills: { title: string }[];
+  id: string;
+  value: string;
+  status: string;
+  questCID: string;
+  orgId: string;
+  deadline: string;
+  blockNumber: number;
 }
 
 const Dashboard: NextPage = () => {
@@ -91,6 +69,23 @@ const Dashboard: NextPage = () => {
       retry: 2,
     }
   );
+  const { data: proposal } = useQuery(
+    ["lancer.proposal", address],
+    () => ProposalService.fetchProposalsFromLancer(`${address}`),
+    {
+      enabled: !!address,
+    }
+  );
+  console.log({ proposal });
+  const pendingProposals = useMemo(() => {
+    if (proposal?.proposals.length) {
+      return proposal.proposals.filter(
+        ({ status }) =>
+          ![ProposalStatus.Awarded, ProposalStatus.Rejected].includes(status)
+      );
+    }
+    return [];
+  }, [proposal]);
 
   if (!isConnected) {
     router.push("/");
@@ -127,8 +122,8 @@ const Dashboard: NextPage = () => {
             <TabPanel>Nothing to see here</TabPanel>
             <TabPanel>
               <Box py="10" px="2">
-                <Accordion allowMultiple allowToggle border="2px solid #E8EDF6">
-                  <AccordionItem mb="50px">
+                <Accordion allowMultiple>
+                  <AccordionItem>
                     <h2>
                       <AccordionButton
                         bg="#321975"
@@ -146,7 +141,7 @@ const Dashboard: NextPage = () => {
                     </AccordionPanel>
                   </AccordionItem>
 
-                  <AccordionItem mb="50px">
+                  <AccordionItem mt="20px">
                     <h2>
                       <AccordionButton
                         bg="#321975"
@@ -161,53 +156,79 @@ const Dashboard: NextPage = () => {
                     </h2>
                     <AccordionPanel bg="#E8EDF6" py={10} px="24" color="black">
                       {credentials?.length ? (
-                        credentials.map(({ id }) => (
-                          <Box
-                            boxShadow="md"
-                            bgGradient="linear(to-b, #7ea1cb, #2c5683)"
-                            borderColor={"#9771ff"}
-                            borderWidth={"5px"}
-                            key={id}
-                            w={"200px"}
-                            h={"300px"}
-                            rounded={"25px"}
-                          >
-                            <VStack pt="50px" px="20px" align={"start"}>
-                              <Logo width="150" height="50" />
-                              <Box rounded={"50px"} minW="150px" bg="#9771ff">
-                                Uhm
+                        credentials.map(
+                          ({ id, orgId, deadline, skills, title }) => {
+                            const { data: organization } = useQuery(
+                              ["organization.id", orgId],
+                              () =>
+                                OrganizationService.findOrganizationById(
+                                  Number(orgId)
+                                ),
+                              {
+                                enabled: !!orgId,
+                              }
+                            );
+                            return (
+                              <Box
+                                boxShadow="md"
+                                bgGradient="linear(to-b, #7ea1cb, #2c5683)"
+                                borderColor={"#9771ff"}
+                                borderWidth={"5px"}
+                                key={id}
+                                w={"200px"}
+                                h={"300px"}
+                                rounded={"25px"}
+                                overflow="hidden"
+                              >
+                                <VStack pt="15px" px="20px" align={"start"}>
+                                  <Logo width="150" height="50" />
+                                  <Box
+                                    rounded={"50px"}
+                                    p="2"
+                                    minW="150px"
+                                    bg="#9771ff"
+                                    noOfLines={1}
+                                  >
+                                    {title}
+                                  </Box>
+                                  <Text
+                                    color="white"
+                                    fontSize={"10px"}
+                                    textAlign="start"
+                                  >
+                                    Issue by: {organization.org.name}
+                                  </Text>
+                                  <Text
+                                    color="white"
+                                    fontSize={"10px"}
+                                    textAlign="start"
+                                  >
+                                    Issue Date:{" "}
+                                    {new Date(deadline).toLocaleDateString()}
+                                  </Text>
+                                  <Text
+                                    color="white"
+                                    fontSize={"10px"}
+                                    textAlign="start"
+                                  >
+                                    Hours Completed: {100}
+                                  </Text>
+                                  <Text
+                                    color="white"
+                                    fontSize={"10px"}
+                                    textAlign="start"
+                                    noOfLines={4}
+                                  >
+                                    Skills:{" "}
+                                    {skills
+                                      .map(({ title }) => title)
+                                      .join(", ")}
+                                  </Text>
+                                </VStack>
                               </Box>
-                              <Text
-                                color="white"
-                                fontSize={"10px"}
-                                textAlign="start"
-                              >
-                                Issue by: Developer DAO
-                              </Text>
-                              <Text
-                                color="white"
-                                fontSize={"10px"}
-                                textAlign="start"
-                              >
-                                Issue Date: 1/2023
-                              </Text>
-                              <Text
-                                color="white"
-                                fontSize={"10px"}
-                                textAlign="start"
-                              >
-                                Hours Completed: 100
-                              </Text>
-                              <Text
-                                color="white"
-                                fontSize={"10px"}
-                                textAlign="start"
-                              >
-                                Skills: Skill 1, Skill, Skill 3
-                              </Text>
-                            </VStack>
-                          </Box>
-                        ))
+                            );
+                          }
+                        )
                       ) : (
                         <Box color="red" textAlign="center">
                           No Quests Completed Yet
@@ -216,7 +237,7 @@ const Dashboard: NextPage = () => {
                     </AccordionPanel>
                   </AccordionItem>
 
-                  <AccordionItem mb="50px">
+                  <AccordionItem mt="20px">
                     <h2>
                       <AccordionButton
                         bg="#321975"
@@ -224,13 +245,92 @@ const Dashboard: NextPage = () => {
                         _hover={{ bg: "#321975" }}
                       >
                         <Box as="span" flex="1" textAlign="left">
-                          Pending
+                          Pending Quests
                         </Box>
                         <AccordionIcon />
                       </AccordionButton>
                     </h2>
                     <AccordionPanel bg="#E8EDF6" py={10} px="24" color="black">
-                      Quest Pending
+                      {pendingProposals.length ? (
+                        pendingProposals.map(({ questId, quest, proposer }) => {
+                          const { data: organization } = useQuery(
+                            ["organization.id", quest.orgId],
+                            () =>
+                              OrganizationService.findOrganizationById(
+                                Number(quest.orgId)
+                              ),
+                            {
+                              enabled: !!quest.orgId,
+                            }
+                          );
+                          const { deadline } = quest;
+                          const { data } = useQuery(
+                            [`Quest-${questId}`, quest.questCID],
+                            () => QuestService.fetchQuestByCID(quest.questCID)
+                          );
+                          return (
+                            <Box
+                              boxShadow="md"
+                              bgGradient="linear(to-b, #7ea1cb, #2c5683)"
+                              borderColor={"#9771ff"}
+                              borderWidth={"5px"}
+                              key={questId}
+                              w={"200px"}
+                              h={"300px"}
+                              rounded={"25px"}
+                              overflow="hidden"
+                            >
+                              <VStack pt="15px" px="20px" align={"start"}>
+                                <Logo width="150" height="50" />
+                                <Box
+                                  rounded={"50px"}
+                                  p="2"
+                                  minW="150px"
+                                  bg="#9771ff"
+                                  noOfLines={1}
+                                >
+                                  {data?.title}
+                                </Box>
+                                <Text
+                                  color="white"
+                                  fontSize={"10px"}
+                                  textAlign="start"
+                                >
+                                  Issue by: {organization.org.name}
+                                </Text>
+                                <Text
+                                  color="white"
+                                  fontSize={"10px"}
+                                  textAlign="start"
+                                >
+                                  Issue Date:{" "}
+                                  {new Date(deadline).toLocaleDateString()}
+                                </Text>
+                                <Text
+                                  color="white"
+                                  fontSize={"10px"}
+                                  textAlign="start"
+                                >
+                                  Hours Completed: {100}
+                                </Text>
+                                <Text
+                                  color="white"
+                                  fontSize={"10px"}
+                                  textAlign="start"
+                                  noOfLines={4}
+                                >
+                                  Skills:{" "}
+                                  {data?.skills
+                                    .map(({ title }: any) => title)
+                                    .join(", ")}
+                                </Text>
+                              </VStack>
+                            </Box>
+                          );
+                        })
+                      ) : (
+                        <Box color="red">There are no Pending Quests</Box>
+                      )}
                     </AccordionPanel>
                   </AccordionItem>
                 </Accordion>
